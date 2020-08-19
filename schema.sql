50e3599351d2aced7a2745aef5b9129bacc97960
Mattias Besemann <mattias.besemann@gmail.com>
2020-08-18 22:13:37 -0400

    Create schema

---
+++ schema.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE employee_tracker;
+
+USE employee_tracker;
+
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE role(
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id),
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id),
+    PRIMARY KEY(id)
+);
